2b574c6d08f81adf676ff751d7fb628424ff994d
Bendzae <Bendzae@gmail.com>
2015-01-28 10:57:09 +0100

    small fix

--- selectionsort.c
+++ selectionsort.c
@@ -7,8 +7,9 @@ void selectionsort(array_t* arr, int (*update)(array_t*,int))
   for(i=0;i<arr->length;i++){
       for(j=i+1;j<arr->length;j++){
            if(arr->ptr[i]>arr->ptr[j]){
-               temp=arr->ptr[i];
+              temp=arr->ptr[i];
               arr->ptr[i]=arr->ptr[j];
+              if( update(arr, i)) return;
               arr->ptr[j]=temp;
               if( update(arr, j)) return;
            }
