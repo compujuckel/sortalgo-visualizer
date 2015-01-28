ee9fed7ff32d52e264e162a8d3a78ab0c684c8bc
Bendzae <Bendzae@gmail.com>
2015-01-28 10:32:18 +0100

    added shellsort

---
+++ shellsort.c
@@ -0,0 +1,29 @@
+#include "bubblesort.h"
+#include "array.h"
+#include "graphics.h"
+
+void ShellSort(array_t* array, int (*update)(array_t*,int))
+{
+  int i, j, increment, temp;
+  for(increment = array->length/2;increment > 0; increment /= 2)
+    {
+      for(i = increment; i<array->length; i++)
+        {
+          temp = array->ptr[i];
+          if(update(array, j)) return;
+          for(j = i; j >= increment ;j-=increment)
+            {
+              if(temp < array->ptr[j-increment])
+                {
+                  array->ptr[j] = array->ptr[j-increment];
+                  if(update(array, j)) return;
+                }
+                else
+                  {
+                    break;
+                  }
+             }
+              array->ptr[j] = temp;
+          }
+      }
+}
---
+++ shellsort.h
@@ -0,0 +1,9 @@
+
+#ifndef SHELLSORT_H
+#define SHELLSORT_H
+
+#include "array.h"
+
+void ShellSort(array_t* a, int (*update)(array_t*,int));
+
+#endif
